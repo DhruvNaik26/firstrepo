34dc1b8d45463b4006f973313af004ebd2cfb850
Dhruv Naik <ndhruv26@gmail.com>
2023-10-25 00:04:32 +0530

    commit on 25/10/2023 on 0004 hours

---
+++ script.js
@@ -0,0 +1,15 @@
+// console.log("heheh");
+// document.write("heheheheheh");
+// console.warn("this warn");
+// console.error("this is error");
+var num1=45;
+var num2=45;
+console.log(num1 +num2);
+console.log("eehe");
+console.log('ddd');
+
+// DOM(Document Object model) manupilation in JavaScript
+let exam = document.getElementById('rep');
+console.log(exam);
+exam.style.background= "red";
+
