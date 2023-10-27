00712d938c936be9ec268da3378c3d075b8d2dce
Dhruv Naik <ndhruv26@gmail.com>
2023-10-27 22:25:10 +0530

    f

--- script.js
+++ script.js
@@ -15,18 +15,25 @@ exam.style.background= "red";
 let bt=document.getElementById('onclick');
 document.getElementById('rep').innertext="hi"
 
-tn=document.getElementsByTagName('div');
-// console.log(tn);
- created_element=document.createElement('p');
+// tn=document.getElementsByTagName('div');
+// // console.log(tn);
+//  created_element=document.createElement('p');
 
-created_element.innerText="THIS IS CREATED USING JAVASCRIPT";
+// created_element.innerText="THIS IS CREATED USING JAVASCRIPT";
 
-tn[0].appendChild(created_element);
+// tn[0].appendChild(created_element);
 
-created_element2=document.createElement('h1');
-created_element2.innerText="adaign by java";
-tn[0].replaceChild(created_element2,created_element);
+// created_element2=document.createElement('h1');
+// created_element2.innerText="adaign by java";
+// tn[0].replaceChild(created_element2,created_element);
 
-function clicked(){
-          document.getElementById('rep').innerText=("u have clicked the button");          
+// function clicked(){
+//           document.getElementById('rep').innerText=("u have clicked the button");          
+// }
+summ=(a,b)=>{
+          return a+b;
 }
+print=()=>{
+          alert("it works");
+}
+// setTimeout(print,2000);
